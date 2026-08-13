771f72f1f9eae9e17596a6f35546b82ade2778e1
douglassg86-ai <douglassg86@gmail.com>
2026-08-13 16:20:21 -0300

    chore: remove /gpitracker route (legacy, superseded by gpi-tracker-app repo)

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import GPITrackerApp from './features/gpitracker/GPITrackerApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/gpitracker/*" element={<GPITrackerApp />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
